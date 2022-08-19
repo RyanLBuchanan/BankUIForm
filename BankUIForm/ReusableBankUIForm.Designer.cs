535feb4bea5c4c3ae8edfa797614cf0c4cf6175a
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-19 08:17:41 -0600

    Add project files.

---
+++ BankUIForm/ReusableBankUIForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace BankUIForm
+{
+    partial class ReusableBankUIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accountLabel = new System.Windows.Forms.Label();
+            this.accountTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.balanceTextBox = new System.Windows.Forms.TextBox();
+            this.balanceLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // accountLabel
+            // 
+            this.accountLabel.AutoSize = true;
+            this.accountLabel.Location = new System.Drawing.Point(56, 56);
+            this.accountLabel.Name = "accountLabel";
+            this.accountLabel.Size = new System.Drawing.Size(86, 25);
+            this.accountLabel.TabIndex = 0;
+            this.accountLabel.Text = "Account";
+            this.accountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // accountTextBox
+            // 
+            this.accountTextBox.Location = new System.Drawing.Point(232, 48);
+            this.accountTextBox.Name = "accountTextBox";
+            this.accountTextBox.Size = new System.Drawing.Size(300, 33);
+            this.accountTextBox.TabIndex = 1;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(230, 133);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(300, 33);
+            this.firstNameTextBox.TabIndex = 3;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(54, 141);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(107, 25);
+            this.firstNameLabel.TabIndex = 2;
+            this.firstNameLabel.Text = "First Name";
+            this.firstNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(230, 218);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(300, 33);
+            this.lastNameTextBox.TabIndex = 5;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(54, 226);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(104, 25);
+            this.lastNameLabel.TabIndex = 4;
+            this.lastNameLabel.Text = "Last Name";
+            this.lastNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // balanceTextBox
+            // 
+            this.balanceTextBox.Location = new System.Drawing.Point(232, 303);
+            this.balanceTextBox.Name = "balanceTextBox";
+            this.balanceTextBox.Size = new System.Drawing.Size(300, 33);
+            this.balanceTextBox.TabIndex = 7;
+            // 
+            // balanceLabel
+            // 
+            this.balanceLabel.AutoSize = true;
+            this.balanceLabel.Location = new System.Drawing.Point(56, 311);
+            this.balanceLabel.Name = "balanceLabel";
+            this.balanceLabel.Size = new System.Drawing.Size(80, 25);
+            this.balanceLabel.TabIndex = 6;
+            this.balanceLabel.Text = "Balance";
+            this.balanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.balanceTextBox);
+            this.Controls.Add(this.balanceLabel);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.accountTextBox);
+            this.Controls.Add(this.accountLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label accountLabel;
+        private System.Windows.Forms.TextBox accountTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox balanceTextBox;
+        private System.Windows.Forms.Label balanceLabel;
+    }
+}
+
---
+++ BankUIForm/ReusableBankUIForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankUIForm
+{
+    public partial class ReusableBankUIForm : Form
+    {
+        protected int TextBoxCount { get; set; } = 4;  // number of TextBoxes
+
+        // Enumeration constants specify Textbox indices
+        public enum TextBoxIndices { Account, First, Last, Balance }
+
+        // Parameterless constructor 
+        public ReusableBankUIForm()
+        {
+            InitializeComponent();
+        }
+
+        // Clear all TextBoxes
+        public void ClearTextBoxes()
+        {
+            // Iterate through every Control on the form
+            foreach (Control guiControl in Controls)
+            {
+                // If Control is a TextBox, clear it
+                (guiControl as TextBox)?.Clear();
+            }
+        }
+
+        // Set TextBox values to string-array values
+        public void SetTextBoxValues(string[] values)
+        {
+            // Determine whether string array has correct length
+            if (values.Length != TextBoxCount)
+            {
+                // Throw exception if not correct length
+                throw (new ArgumentException(
+                    $"There must be {TextBoxCount} strings in the array",
+                    nameof(values)));
+            }
+            else // Set array values if array has correct length
+            {
+                // Set array values to TextBox values
+                accountTextBox.Text = values[(int)TextBoxIndices.Account];
+                firstNameTextBox.Text = values[(int)TextBoxIndices.First];
+                lastNameTextBox.Text = values[(int)TextBoxIndices.Last];
+                balanceTextBox.Text = values[(int)TextBoxIndices.Balance];
+            }
+        }
+
+        // Return TextBox values as string array
+        public string[] GetTextBoxValues()
+        {
+            return new string[]
+            {
+                accountTextBox.Text, firstNameTextBox.Text,
+                lastNameTextBox.Text, balanceTextBox.Text};
+        }
+    }
+}
